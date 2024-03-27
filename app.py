319d1ef2e9ec5c626abd7ddb3bf8350c9533f2a3
franckDev21 <tutodevphp@gmail.com>
2024-03-27 13:16:26 +0100

    update

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def build_conversation_dict(messages: list) -> list[dict]:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=8000)
